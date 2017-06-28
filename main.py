0a32786ef86b10a0959dfe8362ad222c8a525f26
Aman Arya <aman.arya524@gmail.com>
2017-06-28 14:12:08 -0400

    Change unicode input from .json to string

--- main.py
+++ main.py
@@ -31,14 +31,24 @@ def main():
 	with open('config.json') as config_json:
 	    config = json.load(config_json)
 
-	img = nib.load(config['data_file'])
+	data_file = str(config['data_file'])
+	data_bval = str(config['data_bval'])
+	data_bvec = str(config['data_bvec'])
+	print(data_file)
+	print(data_bval)
+	print(data_bvec)
+	print(type(data_file))
+	print(type(data_bvec))
+	print(type(data_bval))
+
+	img = nib.load(data_file)
 
 	print("Calculating DTI...")
 	if not op.exists('./dti_FA.nii.gz'):
-	    dti_params = dti.fit_dti(config['data_file'], config['data_bval'], config['data_bvec'], out_dir='.')
+	    dti_params = dti.fit_dti(data_file, data_bval, data_bvec, out_dir='.')
 	else:
 	    dti_params = {'FA': './dti_FA.nii.gz',
-		          'params': './dti_params.nii.gz'}
+			  'params': './dti_params.nii.gz'}
 
 	tg = nib.streamlines.load('csa_prob.trk').tractogram
 	streamlines = tg.apply_affine(np.linalg.inv(img.affine)).streamlines
@@ -53,28 +63,28 @@ def main():
 	for name in bundle_names:
 	    for hemi in ['_R', '_L']:
 		bundles[name + hemi] = {'ROIs': [templates[name + '_roi1' + hemi],
-		                                 templates[name + '_roi1' + hemi]],
-		                        'rules': [True, True]}
+			                         templates[name + '_roi1' + hemi]],
+			                'rules': [True, True]}
 
 
 	print("Registering to template...")
 	MNI_T2_img = dpd.read_mni_template()
-	bvals, bvecs = read_bvals_bvecs(config['data_bval'], config['data_bvec'])
+	bvals, bvecs = read_bvals_bvecs(data_bval, data_bvec)
 	gtab = gradient_table(bvals, bvecs, b0_threshold=100)
-	mapping = reg.syn_register_dwi(config['data_file'], gtab)
+	mapping = reg.syn_register_dwi(data_file, gtab)
 	reg.write_mapping(mapping, './mapping.nii.gz')
 
 
 	print("Segmenting fiber groups...")
-	fiber_groups = seg.segment(config['data_file'],
-		                   config['data_bval'],
-		                   config['data_bvec'],
-		                   streamlines,
-		                   bundles,
-		                   reg_template=MNI_T2_img,
-		                   mapping=mapping,
-		                   as_generator=False,
-		                   affine=img.affine)
+	fiber_groups = seg.segment(data_file,
+			           data_bval,
+			           data_bvec,
+			           streamlines,
+			           bundles,
+			           reg_template=MNI_T2_img,
+			           mapping=mapping,
+			           as_generator=False,
+			           affine=img.affine)
 
 
 	FA_img = nib.load(dti_params['FA'])
--- tract_profile.py
+++ tract_profile.py
@@ -13,6 +13,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import nibabel as nib
 import dipy.data as dpd
+import json
 from dipy.data import fetcher
 from dipy.io.gradients import read_bvals_bvecs
 from dipy.core.gradients import gradient_table
@@ -26,79 +27,70 @@ import AFQ.segmentation as seg
 
 plt.switch_backend('agg')
 
-data_path = '/N/dc2/projects/lifebid/HCP7/108323/'
-data_file = data_path + 'original_hcp_data/Diffusion_7T/' + 'data.nii.gz'
-data_bvec = data_path + 'original_hcp_data/Diffusion_7T/' + 'bvecs'
-data_bval = data_path + 'original_hcp_data/Diffusion_7T/' + 'bvals'
+def main():
+	with open('config.json') as config_json:
+	    config = json.load(config_json)
 
-#data_brainmask = data_path + 'original_hcp_data/Diffusion_7T/' + 'nodif_brain_mask.nii.gz'
-#fs_path = '/N/dc2/projects/lifebid/HCP/Brent/7t_rerun/freesurfer/7t_108323'
-#data_fs_seg = fs_path + '/mri/aparc+aseg.nii.gz'
-
-"""
-dpd.fetch_stanford_hardi()
-
-hardi_dir = op.join(fetcher.dipy_home, "stanford_hardi")
-hardi_fdata = op.join(hardi_dir, "HARDI150.nii.gz")
-hardi_fbval = op.join(hardi_dir, "HARDI150.bval")
-hardi_fbvec = op.join(hardi_dir, "HARDI150.bvec")
-"""
-
-img = nib.load(data_file)
-
-print("Calculating DTI...")
-if not op.exists('./dti_FA.nii.gz'):
-    dti_params = dti.fit_dti(data_file, data_bval, data_bvec,
-                             out_dir='.')
-else:
-    dti_params = {'FA': './dti_FA.nii.gz',
-                  'params': './dti_params.nii.gz'}
-
-tg = nib.streamlines.load('csa_prob.trk').tractogram
-streamlines = tg.apply_affine(np.linalg.inv(img.affine)).streamlines
-
-# Use only a small portion of the streamlines, for expedience:
-streamlines = streamlines[::100]
-
-templates = afd.read_templates()
-bundle_names = ["CST", "ILF"]
-
-bundles = {}
-for name in bundle_names:
-    for hemi in ['_R', '_L']:
-        bundles[name + hemi] = {'ROIs': [templates[name + '_roi1' + hemi],
-                                         templates[name + '_roi1' + hemi]],
-                                'rules': [True, True]}
+	data_file = config['data_file']
+	data_bval = config['data_bval']
+	data_bvec = config['data_bvec']
 
 
-print("Registering to template...")
-MNI_T2_img = dpd.read_mni_template()
-bvals, bvecs = read_bvals_bvecs(data_bval, data_bvec)
-gtab = gradient_table(bvals, bvecs, b0_threshold=100)
-mapping = reg.syn_register_dwi(data_file, gtab)
-reg.write_mapping(mapping, './mapping.nii.gz')
+	img = nib.load(data_file)
+
+	print("Calculating DTI...")
+	if not op.exists('./dti_FA.nii.gz'):
+	    dti_params = dti.fit_dti(data_file, data_bval, data_bvec,
+		                     out_dir='.')
+	else:
+	    dti_params = {'FA': './dti_FA.nii.gz',
+		          'params': './dti_params.nii.gz'}
+
+	tg = nib.streamlines.load('csa_prob.trk').tractogram
+	streamlines = tg.apply_affine(np.linalg.inv(img.affine)).streamlines
+
+	# Use only a small portion of the streamlines, for expedience:
+	streamlines = streamlines[::100]
+
+	templates = afd.read_templates()
+	bundle_names = ["CST", "ILF"]
+
+	bundles = {}
+	for name in bundle_names:
+	    for hemi in ['_R', '_L']:
+		bundles[name + hemi] = {'ROIs': [templates[name + '_roi1' + hemi],
+		                                 templates[name + '_roi1' + hemi]],
+		                        'rules': [True, True]}
 
 
-print("Segmenting fiber groups...")
-fiber_groups = seg.segment(data_file,
-                           data_bval,
-                           data_bvec,
-                           streamlines,
-                           bundles,
-                           reg_template=MNI_T2_img,
-                           mapping=mapping,
-                           as_generator=False,
-                           affine=img.affine)
+	print("Registering to template...")
+	MNI_T2_img = dpd.read_mni_template()
+	bvals, bvecs = read_bvals_bvecs(data_bval, data_bvec)
+	gtab = gradient_table(bvals, bvecs, b0_threshold=100)
+	mapping = reg.syn_register_dwi(data_file, gtab)
+	reg.write_mapping(mapping, './mapping.nii.gz')
 
 
-FA_img = nib.load(dti_params['FA'])
-FA_data = FA_img.get_data()
+	print("Segmenting fiber groups...")
+	fiber_groups = seg.segment(data_file,
+		                   data_bval,
+		                   data_bvec,
+		                   streamlines,
+		                   bundles,
+		                   reg_template=MNI_T2_img,
+		                   mapping=mapping,
+		                   as_generator=False,
+		                   affine=img.affine)
 
-print("Extracting tract profiles...")
-for bundle in bundles:
-    fig, ax = plt.subplots(1)
-    profile = seg.calculate_tract_profile(FA_data, fiber_groups[bundle])
-    ax.plot(profile)
-    ax.set_title(bundle)
-plt.savefig('bundle.png')
-plt.show()
+
+	FA_img = nib.load(dti_params['FA'])
+	FA_data = FA_img.get_data()
+
+	print("Extracting tract profiles...")
+	for bundle in bundles:
+	    fig, ax = plt.subplots(1)
+	    profile = seg.calculate_tract_profile(FA_data, fiber_groups[bundle])
+	    ax.plot(profile)
+	    ax.set_title(bundle)
+	plt.savefig('bundle.png')
+main()
